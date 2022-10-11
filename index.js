baf6da1f696a97705723c99f59c8d878474adfc0
hummus-erectus <robert.samuel.grayson@gmail.com>
2022-10-11 11:37:32 +0900

    Basic code complete
    No styling

---
+++ index.js
@@ -0,0 +1,22 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+const pw1el = document.getElementById("pw1-el");
+const pw2el = document.getElementById("pw2-el");
+
+function generatePasswords(){
+    pw1el.textContent=randomString();
+    pw2el.textContent=randomString();
+}
+
+function randomString(){
+    let password = "";
+    for (let i = 0; i < 15; i++){
+        let randomIndex = Math.floor(Math.random()*characters.length);
+        password += characters[randomIndex];
+    }
+    return password;
+}
+
+
+
