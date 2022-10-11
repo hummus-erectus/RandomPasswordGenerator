713891ce2db3af8c2e9ad2d9dfcaa8ee7aac99a7
hummus-erectus <robert.samuel.grayson@gmail.com>
2022-10-12 00:24:18 +0900

    Added checkboxes

--- index.js
+++ index.js
@@ -2,24 +2,46 @@
 
     "use strict";
     
-    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-    "/"];
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?"];
+
+    let characters = [];
+
+  
 
     const pw1el = document.getElementById("pw1-el");
     const pw2el = document.getElementById("pw2-el");
+    const checkNum = document.getElementById("check-num");
+    const checkSym = document.getElementById("check-sym");
     const btn = document.getElementById("generateButton");
     const pwdMsg = document.getElementById("copyMsg");
 
     btn.addEventListener('click', function (event){
         event.preventDefault;
         let userLength = document.querySelector("#passwordLength").value;
+
+        characters=[];
+        characters.push(...alphabet);
+
+        if (checkNum.checked===true){
+            characters.push(...numbers);
+        }
+        if (checkSym.checked===true){
+            characters.push(...symbols);
+        }
+
         pw1el.textContent=randomString(userLength);
         pw2el.textContent=randomString(userLength);
         pwdMsg.textContent="Click a password to copy it to the clipboard";
+   
     });
 
     function randomString(length){
         let password = "";
+
         for (let i = 0; i < length; i++){
             let randomIndex = Math.floor(Math.random()*characters.length);
             password += characters[randomIndex];
@@ -33,7 +55,7 @@
         event.preventDefault;
         if(pw1el.textContent){
             const cb = navigator.clipboard;
-            cb.writeText(pw1el.innerText).then(() => alert('Text copied'));
+            cb.writeText(pw1el.innerText).then(() => alert('Pasword copied to clipboard'));
         }
     });
 
